13553ec5371a484d6f0efb9a2949efd6c0a45124
Joe Clarke <joe@japh.co.uk>
2024-10-12 17:31:24 +0100

    Dual lines - configurable colour

--- vga_driver/src/top_level.vhd
+++ vga_driver/src/top_level.vhd
@@ -10,11 +10,14 @@ entity top_vga_driver is
     Port (
         clk : in std_logic;
 
-        vgaRed : out std_logic_vector(3 downto 0);
+        sw   : in std_logic_vector(15 downto 0);
+        btnC : in std_logic;
+
+        vgaRed   : out std_logic_vector(3 downto 0);
         vgaGreen : out std_logic_vector(3 downto 0);
-        vgaBlue : out std_logic_vector(3 downto 0);
-        Hsync : out std_logic;
-        Vsync : out std_logic
+        vgaBlue  : out std_logic_vector(3 downto 0);
+        Hsync    : out std_logic;
+        Vsync    : out std_logic
     );
 end top_vga_driver;
 
@@ -22,11 +25,13 @@ architecture rtl of top_vga_driver is
 
 CONSTANT H_LIMIT    : natural := 800;
 CONSTANT H_RES      : natural := 640;
-CONSTANT H_L_BORDER : natural := 144;
+CONSTANT H_L_BORDER : natural := 148;
+CONSTANT H_R_BORDER : natural := 12;
 
 CONSTANT V_LIMIT    : natural := 525;
 CONSTANT V_RES      : natural := 480;
 CONSTANT V_T_BORDER : natural := 35;
+CONSTANT V_B_BORDER : natural := 10;
 
 signal reset : std_logic;
 
@@ -34,7 +39,7 @@ signal pixel_clk        : std_logic;
 signal pixel_clk_locked : std_logic;
 signal pixel_clk_fb : std_logic;
 
-signal twenty_five_hz_counter : unsigned(31 downto 0);
+signal fifty_hz_counter : unsigned(31 downto 0);
 
 signal h_counter : unsigned(9 downto 0);
 signal v_counter : unsigned(9 downto 0);
@@ -45,51 +50,44 @@ signal y_pos : unsigned(10 downto 0);
 signal x_inc : signed(1 downto 0);
 signal y_inc : signed(1 downto 0);
 
+signal rgb_colour : std_logic_vector(11 downto 0);
+
 begin
 
--- PLLE2_BASE: Base Phase Locked Loop (PLL)
---             7 Series
--- Xilinx HDL Language Template, version 2024.1
+------------------------------------------------------------
+-- Generate 25 MHz clock for use as pixel clock
+------------------------------------------------------------
 
-PLLE2_BASE_inst : PLLE2_BASE
+pixel_clock_gen_inst : PLLE2_BASE
 generic map (
-   BANDWIDTH            => "OPTIMIZED",  -- OPTIMIZED, HIGH, LOW
-   CLKFBOUT_MULT        => 8,        -- Multiply value for all CLKOUT, (2-64)
-   CLKOUT0_DIVIDE       => 32,
-
-   CLKFBOUT_PHASE       => 0.0,     -- Phase offset in degrees of CLKFB, (-360.000-360.000).
-   CLKIN1_PERIOD        => 10.0,      -- Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
-   -- CLKOUT0_DIVIDE - CLKOUT5_DIVIDE: Divide amount for each CLKOUT (1-128)
-
-   -- CLKOUT0_DUTY_CYCLE - CLKOUT5_DUTY_CYCLE: Duty cycle for each CLKOUT (0.001-0.999).
-   CLKOUT0_DUTY_CYCLE   => 0.5,
-
-   -- CLKOUT0_PHASE - CLKOUT5_PHASE: Phase offset for each CLKOUT (-360.000-360.000).
-   CLKOUT0_PHASE        => 0.0,
-
-   DIVCLK_DIVIDE        => 1,        -- Master division value, (1-56)
-   REF_JITTER1          => 0.0,        -- Reference input jitter in UI, (0.000-0.999).
-   STARTUP_WAIT         => "FALSE"    -- Delay DONE until PLL Locks, ("TRUE"/"FALSE")
+    CLKIN1_PERIOD        => 10.0,      -- Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
+    CLKFBOUT_MULT        => 8,        -- Multiply value for all CLKOUT, (2-64)
+    CLKOUT0_DIVIDE       => 32,
+    DIVCLK_DIVIDE        => 1        -- Master division value, (1-56)
 )
 port map (
-   -- Clock Outputs: 1-bit (each) output: User configurable clock outputs
-   CLKOUT0  => pixel_clk,   -- 1-bit output: CLKOUT0
+    -- Clock Input
+    CLKIN1   => clk,
+    -- Clock Output
+    CLKOUT0  => pixel_clk,
 
-   -- Feedback Clocks: 1-bit (each) output: Clock feedback ports
-   CLKFBOUT => pixel_clk_fb, -- 1-bit output: Feedback clock
-   LOCKED   => pixel_clk_locked,     -- 1-bit output: LOCK
-   CLKIN1   => clk,     -- 1-bit input: Input clock
-   -- Control Ports: 1-bit (each) input: PLL control ports
-   PWRDWN   => '0',     -- 1-bit input: Power-down
-   RST      => '0',           -- 1-bit input: Reset
-   -- Feedback Clocks: 1-bit (each) input: Clock feedback ports
-   CLKFBIN  => pixel_clk_fb    -- 1-bit input: Feedback clock
+   -- Feedback Clock
+   CLKFBOUT => pixel_clk_fb,
+   CLKFBIN  => pixel_clk_fb,    -- 1-bit input: Feedback clock
+
+   -- Control Ports
+   PWRDWN   => '0',
+   RST      => '0',
+
+   -- Locked Output
+   LOCKED   => pixel_clk_locked
 );
 
 -- End of PLLE2_BASE_inst instantiation
 
 reset <= not pixel_clk_locked;
 
+-- Counters for timing H-Sync and V-Sync
 h_v_counters_proc : process(pixel_clk) is
 begin
     if rising_edge(pixel_clk) then
@@ -111,6 +109,7 @@ begin
     end if;
 end process;
 
+-- Driving H-Sync and V-Sync based on counters
 h_v_sync_proc : process(pixel_clk) is
 begin
     if rising_edge(pixel_clk) then
@@ -135,26 +134,28 @@ begin
     end if;
 end process;
 
+-- Timer for speed of line movement
 timer_proc : process(pixel_clk) is
 begin
     if rising_edge(pixel_clk) then
-        twenty_five_hz_counter <= twenty_five_hz_counter + 1;
+        fifty_hz_counter <= fifty_hz_counter + 1;
 
-        if twenty_five_hz_counter = 1000000 then
-            twenty_five_hz_counter <= (others => '0');
+        if fifty_hz_counter = 500000 then
+            fifty_hz_counter <= (others => '0');
         end if;
 
         if reset = '1' then
-            twenty_five_hz_counter <= (others => '0');
+            fifty_hz_counter <= (others => '0');
         end if;
     end if;
 end process;
 
+-- Generate bouncing x and y positions
 xy_proc : process(pixel_clk) is
 begin
     if rising_edge(pixel_clk) then
 
-        if twenty_five_hz_counter = 0 then
+        if fifty_hz_counter = 0 then
             x_pos <= unsigned(signed(x_pos) + x_inc);
             y_pos <= unsigned(signed(y_pos) + y_inc);
         end if;
@@ -167,10 +168,10 @@ begin
         if (y_pos = 0) or (y_pos = V_RES - 1) then
             y_inc <= -y_inc;
             y_pos <= unsigned(signed(y_pos) + y_inc * 2);
-            end if;
+        end if;
 
 
-        if reset = '1' then
+        if (reset = '1') or (btnC = '1') then
             x_pos <= to_unsigned(H_RES / 2, x_pos'length);
             y_pos <= to_unsigned(V_RES / 2, y_pos'length);
             x_inc <= to_signed(1, x_inc'length);
@@ -180,21 +181,25 @@ begin
     end if;
 end process;
 
-rgb_proc : process(pixel_clk) is
+-- Draw lines at the x and y positions
+line_drawing_proc : process(pixel_clk) is
 begin
     if rising_edge(pixel_clk) then
-        if (h_counter = x_pos + H_L_BORDER) or (v_counter = y_pos + V_T_BORDER) then
-            vgaBlue <= "1111";
+        if (h_counter = x_pos + H_L_BORDER) or (v_counter = y_pos + V_T_BORDER) -- Line for pos x & y
+        or (h_counter = H_LIMIT - H_R_BORDER - x_pos) or (v_counter = V_LIMIT - V_B_BORDER - y_pos) then -- Line for neg x & y
+            rgb_colour <= sw(rgb_colour'range);
         else
-            vgaBlue <= "0000";
+            rgb_colour <= (others => '0');
         end if;
 
         if reset = '1' then
-            vgaRed   <= (others => '0');
-            vgaBlue  <= (others => '0');
-            vgaGreen <= (others => '0');
+            rgb_colour <= (others => '0');
         end if;
     end if;
 end process;
+
+vgaRed   <= rgb_colour(11 downto 8);
+vgaGreen <= rgb_colour(7 downto 4);
+vgaBlue  <= rgb_colour(3 downto 0);
 
 end rtl;
